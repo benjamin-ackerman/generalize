ade4b3237ac9ee9275fa33af259d253935eb516c
Backerman150 <backer10@jhu.edu>
2018-03-06 11:07:34 -0500

    function ideas

---
+++ R/function ideas.R
@@ -0,0 +1,18 @@
+##### Function Ideas: #####
+
+# 1: Trim target population to match covariate bounds of trial
+# 2: Calculate trial participation probabilities using
+      # GLM
+      # Random Forests
+      # Super Learner?
+      # Lasso
+      # others?
+# 3: Compare distributions of trial participation probabilities using
+      # Tipton generalizability index
+      # Kolmogorov-Smirnov?
+      # others?
+# 4: Generalize results using
+      # Weighting by the odds
+      # Weighting by the inverse probability (?)
+      # BART
+      # TMLE
