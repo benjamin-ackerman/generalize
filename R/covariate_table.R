4cfff7cbea8af1649efd9d1ebd3bb730edca92b2
Benjamin Ackerman <backer10@jhu.edu>
2019-05-06 12:24:34 -0400

    fixed covaraite_table to handle data frames

--- R/covariate_table.R
+++ R/covariate_table.R
@@ -8,23 +8,24 @@ covariate_table <- function(trial, selection_covariates, data, weighted_table = 
 
   if(weighted_table == FALSE){
     data = data %>%
-      tidyr::drop_na(selection_covariates)
+      tidyr::drop_na(selection_covariates) %>%
+      as.data.frame()
 
     expanded.data = data.frame(trial = data[, trial],
                                model.matrix(~-1 + ., data = data[, selection_covariates]))
 
     means.tab = expanded.data %>%
-      dplyr::group_by_(trial) %>%
-      dplyr::summarise_at(selection_covariates, mean) %>%
+      dplyr::group_by(trial) %>%
+      dplyr::summarise_at(names(expanded.data)[-1], mean) %>%
       t() %>% as.data.frame()
 
     means.tab = means.tab[-1,]
 
     names(means.tab) = c("trial", "population")
-    n_trial = as.numeric(table(expanded.data[,trial]))[2]
-    n_pop = as.numeric(table(expanded.data[,trial]))[1]
+    n_trial = as.numeric(table(expanded.data[,"trial"]))[2]
+    n_pop = as.numeric(table(expanded.data[,"trial"]))[1]
     sd.tab = expanded.data %>%
-      dplyr::group_by_(trial) %>%
+      dplyr::group_by(trial) %>%
       dplyr::summarise_all(var) %>% t() %>% as.data.frame() %>% .[-1,] %>%
       mutate(pooled_sd = sqrt(((n_trial - 1) * V1 + (n_pop - 1) * V2)/(n_trial + n_pop - 2)))
     names(sd.tab) = c("trial_var", "population_var", "pooled_sd")
@@ -32,7 +33,8 @@ covariate_table <- function(trial, selection_covariates, data, weighted_table = 
 
   if(weighted_table == TRUE){
     data = data %>%
-      tidyr::drop_na(selection_covariates)
+      tidyr::drop_na(selection_covariates) %>%
+      as.data.frame()
 
     data$weights = weighting(outcome = NULL, treatment = NULL, trial = trial,
                              selection_covariates = selection_covariates, data = data,
@@ -42,8 +44,8 @@ covariate_table <- function(trial, selection_covariates, data, weighted_table = 
     expanded.data = data.frame(trial = data[,trial], model.matrix(~ -1 + ., data = data[,c(selection_covariates,"weights")]))
 
     means.tab = expanded.data %>%
-      dplyr::group_by_(trial) %>%
-      dplyr::summarise_at(vars(1:ncol(expanded.data)), funs(weighted.mean(., weights))) %>%
+      dplyr::group_by(trial) %>%
+      dplyr::summarise_at(names(expanded.data)[-1], funs(weighted.mean(., weights))) %>%
       dplyr::select(-`weights`) %>%
       t() %>%
       as.data.frame() %>% .[-1,]
@@ -54,8 +56,8 @@ covariate_table <- function(trial, selection_covariates, data, weighted_table = 
     n_pop = as.numeric(table(expanded.data$trial))[1]
 
     sd.tab = expanded.data %>%
-      dplyr::group_by_(trial) %>%
-      dplyr::summarise_at(vars(1:ncol(expanded.data)),
+      dplyr::group_by(trial) %>%
+      dplyr::summarise_at(names(expanded.data)[-1],
                           funs(sum(weights * (. - weighted.mean(.,weights))^2)/sum(weights))) %>%
       dplyr::select(-`weights`) %>%
       t() %>%
@@ -71,12 +73,12 @@ covariate_table <- function(trial, selection_covariates, data, weighted_table = 
     dplyr::select(trial, population, ASMD)
 
   if(weighted_table == FALSE){
-    row.names(covariate_table) = setdiff(names(expanded.data),c(trial))
+    row.names(covariate_table) = setdiff(names(expanded.data),c("trial"))
   }
 
   if(weighted_table == TRUE){
     names(covariate_table)[1] = "trial (weighted)"
-    row.names(covariate_table) = setdiff(names(expanded.data),c(trial,"weights"))
+    row.names(covariate_table) = setdiff(names(expanded.data),c("trial","weights"))
   }
 
   return(covariate_table)
